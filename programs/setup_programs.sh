b155b58af799f25d0d5cb26a1ed919145bf7fd0e
Mateo <mateo.chavez@hotmail.com>
2019-04-04 21:22:27 +0200

    Added more i3 related programs

--- programs/setup_programs.sh
+++ programs/setup_programs.sh
@@ -4,11 +4,26 @@
 # Display cool folder-trees
 sudo apt-get install tree
 
+
 #######################################################
 #                 I3 RELATED PROGRAMS
 #######################################################
 
+# aranda: monitor configuration, currently not used
+sudo apt-get install aranda
+
+# feh: setup wallpaper on screen
+sudo apt-get install feh
+
+# lxappearance: configure gtk fonts
 sudo apt-get install lxappearance
+
+# playerctl: control media and volume keys
+sudo dpkg -i dpkg_files/playerctl-2.0.2_amd64.deb
+
+# xbacklight: increase and decrease screen brightness
+sudo apt-get install xbacklight
+
 
 
 #######################################################
