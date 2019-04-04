c157f40c1b2f228e3297ade664b57b51d1443f75
Mateo <mateo.chavez@hotmail.com>
2019-04-04 21:24:37 +0200

    Moved all program installation from i3 to "programs"

--- i3/ext/setup_i3ext.sh
+++ i3/ext/setup_i3ext.sh
@@ -1,20 +1,7 @@
 #!/usr/bin/env bash
 
 
-# Playerctl: control media and volume keys
-#sudo dpkg -i playerctl-2.0.2_amd64.deb
-
-# xbacklight: increase and decrease screen brightness
-#sudo apt-get install xbacklight
-
-# feh: setup wallpaper on screen
-#sudo apt-get install feh
-
-# aranda: monitor configuration, currently not used
-#sudo apt-get install aranda
-
-# Get fontawesome
-# Google for fontawesome cheat sheet
+# Place fonts
 mkdir ~/.fonts
 cp ./fontawesome/* ~/.fonts/
 cp ./font_system_san_francisco/* ~/.fonts/
