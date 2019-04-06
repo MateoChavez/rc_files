77d9aff19cf85146e2dfa0feca8072ff3f7bfb73
Mateo <mateo.chavez@hotmail.com>
2019-04-06 09:46:09 +0200

    Added thunar file explorer and arc-theme

--- programs/setup_programs.sh
+++ programs/setup_programs.sh
@@ -12,6 +12,14 @@ sudo apt-get install tree
 # aranda: monitor configuration, currently not used
 sudo apt-get install aranda
 
+# arc-theme: gtk theme
+sudo apt-get install libgtk-3-dev
+git clone https://github.com/horst3180/arc-theme --depth 1 ~/repositories/git/arc-theme
+cd ~/repositories/git/arc-theme
+./autogen.sh --prefix=/usr
+sudo make install
+cd -
+
 # feh: setup wallpaper on screen
 sudo apt-get install feh
 
@@ -20,6 +28,9 @@ sudo apt-get install lxappearance
 
 # playerctl: control media and volume keys
 sudo dpkg -i dpkg_files/playerctl-2.0.2_amd64.deb
+
+# thunar: alternative file explorer since nautilus doesn't work
+sudo apt-get install thunar
 
 # xbacklight: increase and decrease screen brightness
 sudo apt-get install xbacklight
