c01c5a75f7a650033e7b7e3d3d2fe1f91bc4dc22
Mateo <mateo.chavez@hotmail.com>
2019-04-23 20:50:12 +0200

    i3: Added functionality to swap keybord keys to avoid conflicts with alt

--- i3/set_i3_conf.sh
+++ i3/set_i3_conf.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env sh
 
 
-# Copy the main config file
+# copy the main config file
 cp config ~/.config/i3/
 
-# Copy status bar config file
+# copy status bar config file
 cp i3blocks.conf ~/.config/i3/
 
-# Copy font definitions
+# copy font definitions
 cp gtkrc-2.0 ~/.gtkrc-2.0
 cp settings.ini ~/.config/gtk-3.0/setting.ini
+
+# copy the keyboard mapping files
+mkdir ~/.xkb
+mkdir ~/.xkb/symbols
+mkdir ~/.xkb/keymap
+
+cp xkb/i3swap ~/.xkb/symbols/.
+cp xkb/i3kbd ~/.xkb/keymap/.
 
