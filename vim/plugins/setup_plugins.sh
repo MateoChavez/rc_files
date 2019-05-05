33882a919e38e3f535b59c140a5db6d90b6ff276
Mateo <mateo.chavez@hotmail.com>
2019-05-05 21:23:16 +0200

    vim: added emmet plugin to setup script

--- vim/plugins/setup_plugins.sh
+++ vim/plugins/setup_plugins.sh
@@ -7,3 +7,7 @@ mkdir ~/.vim/bundle
 # set up pathogen for plugin management
 cp pathogen.vim ~/.vim/autoload/
 
+# setup emmet for html tags
+mkdir ~/.vim/bundle/emmet
+unzip emmet-vim.zip -d ~/.vim/bundle/emmet/
+
