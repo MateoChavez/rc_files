a3239e35f301d8cc8ff9a991ad8086e2ef51a1c4
Mateo <mateo.chavez@hotmail.com>
2019-07-27 10:59:50 +0200

    vim: added surround plugin

--- vim/plugins/setup_plugins.sh
+++ vim/plugins/setup_plugins.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-# create directories for plugins
-mkdir ~/.vim/autoload
-mkdir ~/.vim/bundle
+## create directories for plugins
+#mkdir ~/.vim/autoload
+#mkdir ~/.vim/bundle
+#
+## set up pathogen for plugin management
+#cp pathogen.vim ~/.vim/autoload/
+#
+## setup emmet for html tags
+#mkdir ~/.vim/bundle/emmet
+#unzip emmet-vim.zip -d ~/.vim/bundle/emmet/
 
-# set up pathogen for plugin management
-cp pathogen.vim ~/.vim/autoload/
-
-# setup emmet for html tags
-mkdir ~/.vim/bundle/emmet
-unzip emmet-vim.zip -d ~/.vim/bundle/emmet/
+# setup surround vim
+unzip vim-surround-master.zip -d ~/.vim/bundle/
+mv ~/.vim/bundle/vim-surround-master ~/.vim/bundle/surround/
 
