79ec8d6abda569d0a184cd1663b1dc3e9892dc22
Mateo <mateo.chavez@hotmail.com>
2019-08-17 20:42:59 +0200

    vim: removed commented lines from setup_plugins.sh

--- vim/plugins/setup_plugins.sh
+++ vim/plugins/setup_plugins.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-## create directories for plugins
-#mkdir ~/.vim/autoload
-#mkdir ~/.vim/bundle
-#
-## set up pathogen for plugin management
-#cp pathogen.vim ~/.vim/autoload/
-#
-## setup emmet for html tags
-#mkdir ~/.vim/bundle/emmet
-#unzip emmet-vim.zip -d ~/.vim/bundle/emmet/
+# create directories for plugins
+mkdir ~/.vim/autoload
+mkdir ~/.vim/bundle
+
+# set up pathogen for plugin management
+cp pathogen.vim ~/.vim/autoload/
+
+# setup emmet for html tags
+mkdir ~/.vim/bundle/emmet
+unzip emmet-vim.zip -d ~/.vim/bundle/emmet/
 
 # setup surround vim
 unzip vim-surround-master.zip -d ~/.vim/bundle/
